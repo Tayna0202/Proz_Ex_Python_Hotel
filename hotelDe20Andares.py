2555dd9f87453ba3214e5d5aea6105d5ae894e54
Tayna0202 <taytv22@gmail.com>
2024-05-16 21:24:27 -0300

    update

--- hotelDe20Andares.py
+++ hotelDe20Andares.py
@@ -5,11 +5,11 @@ print("Usando o laço de repetição FOR")
 print("Hotel de 20 andares...")
 print("Fazendo a contagem de baixo para cima...")
 
-for i in range(20, 13, -1):
-    print("Hotel andar: " + str(i))
-
-for i in range(12, 0, -1):
-    print("Hotel andar: " + str(i))
+for i in range(20, 0, -1):
+    if(i == 13):
+        continue
+    else:
+        print("Hotel andar: " + str(i))
 
 # Repetição While
 
@@ -17,18 +17,9 @@ print("------------------------------------------")
 
 print("Usando o laço de repetição WHILE")
 
-andar = 20
+contador = 20
 
-while(andar > 0):
-    print("Hotel andar: " + str(andar))
-    andar -= 1
-    if(andar <= 13):
-        andar -= 1 
-        break
-
-while(andar > 0):
-    print("Hotel andar: " + str(andar))
-    andar -= 1
-    if(andar <= 0): 
-        break
-     
+while(contador >= 0):
+    if(contador != 13):
+        print("Hotel andar: " + str(contador))
+    contador -= 1
